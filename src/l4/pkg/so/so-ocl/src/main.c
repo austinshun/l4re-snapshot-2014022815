8213ffa084e822af550d6c14ad7c12af39a131fb
Min Xu <minx@ida.ing.tu-bs.de>
2014-05-07 15:08:26 +0200

    MAKE

--- src/l4/pkg/so/so-ocl/src/main.c
+++ src/l4/pkg/so/so-ocl/src/main.c
@@ -1,3 +1,18 @@
+/******************************************************************/
+/*  SOLAR Orbiter DPU System under CCC Framework                  */
+/* -------------------------------------------------------------- */
+/*  Application   SO-OCL                                          */
+/*  Author        Min Xu                                          */
+/*  Date          01.05.2014                                      */
+/*  Version       1.0                                             */
+/* -------------------------------------------------------------- */
+/*  Technical University of Braunschweig, Germany                 */
+/*  Institute for Computer and Network Engineering (IDA)          */
+/* -------------------------------------------------------------- */
+/*  Revision History:                                             */
+/*  1.0 01052014        File Created                              */
+/******************************************************************/
+
 #include <stdio.h>
 #include <unistd.h>
 #include <semaphore.h>
